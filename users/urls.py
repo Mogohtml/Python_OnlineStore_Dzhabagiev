8a162d922832ebdc0f0d182a9d7c0514d8540842
basir <m-basir@bk.ru>
2025-05-20 10:30:47 +0300

    feat: Implemented the functionality for user authentication, login, log-out, and the homepage.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from users.views import product_list, product_detail, category_list, category_detail, inventory_list, inventory_detail, \
-    shopping_cart_list, shopping_cart_detail, order_list, order_detail
+from users.views import register, product_list, product_detail, category_list, category_detail, inventory_list, \
+    inventory_detail, \
+    shopping_cart_list, shopping_cart_detail, order_list, order_detail, home_view
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+path('home/', home_view, name='home'),
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('products/', product_list, name='product_list'),
     path('products/<int:pk>/', product_detail, name='product_detail'),
     path('categories/', category_list, name='products_list'),
--- users/views.py
+++ users/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Product, Category, Customer, Inventory, ShoppingCart, Order
 from .forms import CartForm, OrderForm, ProductForm, CategoryForm, CustomerForm, InventoryForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 # Create your views here.
